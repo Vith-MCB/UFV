857682744948607db2976ffc27438d2df32e4e3b
Vith-MCB <vitorriblacerda@gmail.com>
2021-10-06 17:20:34 -0300

    Conf. C

---
+++ UFV---C/cmake-build-debug/CMakeFiles/UFV___C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UFV___C.dir/main.c.obj"
+  "UFV___C.exe"
+  "UFV___C.exe.manifest"
+  "UFV___C.pdb"
+  "libUFV___C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/UFV___C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
